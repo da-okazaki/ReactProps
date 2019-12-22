43fab0c878c9822dad8d44367361a10eb7d91f44
da-okazaki <35363213+da-okazaki@users.noreply.github.com>
2019-12-22 18:04:31 +0900

    [add] CSS 適用

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,37 +3,16 @@ import React from "react";
 export default function Card(props) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avartar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-
-      {/* 
-      <h2>Beyonce</h2>
-
-      <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        alt="avatar_img"
-      />
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
-
-      <h2>Jack Bauer</h2>
-      <img
-        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        alt="avatar_img"
-      />
-      <p>+987 654 321</p>
-      <p>jack@nowhere.com</p>
-
-      <h2>Chuck Norris</h2>
-      <img
-        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        alt="avatar_img"
-      />
-      <p>+918 372 574</p>
-      <p>gmail@chucknorris.com</p>
-      */}
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img" src={props.img} alt="avartar_img" />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
     </div>
   );
 }
